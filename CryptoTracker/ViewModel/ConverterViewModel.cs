e7d4cc2313fa90babe481346fd17124d579a6f80
Arsen Kovalchuk <kovalchuk.arsen1@gmail.com>
2023-06-15 12:14:10 +0300

    Added cryptocurrency conversion

--- CryptoTracker/ViewModel/ConverterViewModel.cs
+++ CryptoTracker/ViewModel/ConverterViewModel.cs
@@ -12,25 +12,69 @@ namespace CryptoTracker.ViewModel
 {
     public class ConverterViewModel : MainViewModel
     {           
-        private Rates _convertFrom;
-        public Rates ConvertFrom 
+        private CryptoCurrency _convertFrom;
+        public CryptoCurrency ConvertFrom 
         {
             get => _convertFrom;
             set
             {
                 _convertFrom = value;
+                Converter();
                 OnPropertyChanged();
             }
         }
-        private Rates _convertTo;
-        public Rates ConvertTo 
+        private CryptoCurrency _convertTo;
+        public CryptoCurrency ConvertTo 
         {
             get => _convertTo;
             set
             {
                 _convertTo = value;
+                Converter();
                 OnPropertyChanged();
             }
+        }
+        private decimal _leftNumber = 1;
+        public decimal LeftNumber
+        {
+            get => _leftNumber;
+            set
+            {
+                _leftNumber = value;
+                Converter();
+                OnPropertyChanged();
+            }
+        } 
+        private decimal _rightNumber;
+        public decimal RightNumber
+        {
+            get => _rightNumber;
+            set
+            {
+                _rightNumber = value;
+                OnPropertyChanged();
+            }
+        }
+        private decimal _amount = 0.56m;
+        public decimal Amount
+        {
+            get => _amount;
+            set
+            {
+                _amount = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private void Converter()
+        {
+            //if (LeftNumber <= 0 || RightNumber <= 0)
+            //    return;
+            if (ConvertFrom is null || ConvertTo is null)
+                return;
+            Amount = (decimal)ConvertFrom.PriceUsd / (decimal)ConvertTo.PriceUsd;
+            RightNumber = (LeftNumber * (decimal)ConvertFrom.PriceUsd) / (decimal)ConvertTo.PriceUsd;
+            //return Amount;
         }
         public ConverterViewModel()
         {
